f07226470ee4e8e5a0817dd7da89ada47229ec1a
Martin Dilger <martin@nebulit.de>
2026-01-18 00:29:55 +0100

    switch to event processing

--- src/main/kotlin/de/eventmodelers/catalog/addmissingdata/internal/ItemsFetcherProcessor.kt
+++ src/main/kotlin/de/eventmodelers/catalog/addmissingdata/internal/ItemsFetcherProcessor.kt
@@ -5,8 +5,11 @@ import de.eventmodelers.catalog.domain.commands.addmissingdata.AddMissingDataCom
 import de.eventmodelers.catalog.itemdetailstofetch.ItemDetailsToFetchReadModel
 import de.eventmodelers.catalog.itemdetailstofetch.ItemDetailsToFetchReadModelQuery
 import de.eventmodelers.common.Processor
+import de.eventmodelers.events.CatalogueEntryCreatedEvent
 import mu.KotlinLogging
 import org.axonframework.commandhandling.gateway.CommandGateway
+import org.axonframework.eventhandling.DisallowReplay
+import org.axonframework.eventhandling.EventHandler
 import org.axonframework.queryhandling.QueryGateway
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
@@ -15,36 +18,33 @@ import org.springframework.stereotype.Component
 /*
 Boardlink: https://miro.com/app/board/uXjVJo5Vvho=/?moveToWidget=3458764655839872707
 */
+@DisallowReplay
 @Component
 class ItemsFetcherProcessor(val fetchExternalDataFromWarehouse: FetchExternalDataFromWarehouse) :
     Processor {
-  var logger = KotlinLogging.logger {}
+    var logger = KotlinLogging.logger {}
 
-  @Autowired lateinit var commandGateway: CommandGateway
+    @Autowired
+    lateinit var commandGateway: CommandGateway
 
-  @Autowired lateinit var queryGateway: QueryGateway
+    @Autowired
+    lateinit var queryGateway: QueryGateway
 
-  // runs every 60 seconds
-  @Scheduled(fixedDelay = 10000)
-  fun process() {
+    // runs every 60 seconds
+    @EventHandler
+    fun on(event: CatalogueEntryCreatedEvent) {
 
-    // query all TODO Items
-    queryGateway
-        .query(ItemDetailsToFetchReadModelQuery(), ItemDetailsToFetchReadModel::class.java)
-        .thenAccept {
-          // iterate over all items
-          it.data.forEach {
-            // external systems call
-            val bookDetails = fetchExternalDataFromWarehouse.fetch(it.itemId)
+        // external systems call
+        val bookDetails = fetchExternalDataFromWarehouse.fetch(event.itemId)
 
-            // fire command for each item
-            commandGateway.send<AddMissingDataCommand>(
-                AddMissingDataCommand(
-                    itemId = it.itemId,
-                    bookDetails.title,
-                    bookDetails.author,
-                    bookDetails.description))
-          }
-        }
-  }
+        // fire command
+        commandGateway.send<AddMissingDataCommand>(
+            AddMissingDataCommand(
+                itemId = event.itemId,
+                bookDetails.title,
+                bookDetails.author,
+                bookDetails.description
+            )
+        )
+    }
 }
